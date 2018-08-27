06939e310ca783ce932c7434717a13cf089557ec
Trugamr <Trugamr@users.noreply.github.com>
2018-08-27 20:54:56 +0530

    added one dimensional array operations

---
+++ Java/Operations on Matrix/MatrixOperations.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.io.PrintStream;
+
+class MatrixOperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PrintStream ps = new PrintStream(System.out);
+
+        int[][] matrix = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        
+    }
+}
